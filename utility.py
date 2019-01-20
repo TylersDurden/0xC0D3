ef7ac2bdc5e006fc34d9e71d59a80810deb9f46b
TylersDurden <srobbin1@ithaca.edu>
2019-01-20 02:32:35 -0500

    code execution util

---
+++ utility.py
@@ -0,0 +1,21 @@
+import os
+
+
+def swap(fname, destroy):
+    data = []
+    for line in open(fname, 'r').readlines():
+        data.append(line.replace('\n', ''))
+    if destroy:
+        os.remove(fname)
+    return data
+
+
+def write(fname, data):
+    for buffer in data:
+        open(fname, 'w').write(buffer)
+
+
+def execute(command):
+    os.system(command+'>> command.txt')
+    return swap('command.txt', True)
+
